d2ba1188f2f085ea3d860017c084aebefe79209a
satishrajc <satishrajc@gmail.com>
2021-10-15 22:22:54 +0530

    backup schedule

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM openjdk:8-jdk-alpine
+#FROM openjdk:8-jdk-alpine
+FROM adoptopenjdk/openjdk11:jre-11.0.9.1_1-alpine@sha256:b6ab039066382d39cfc843914ef1fc624aa60e2a16ede433509ccadd6d995b1f
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} smart-accounting-book-backend.jar
 ENTRYPOINT ["java","-jar","/smart-accounting-book-backend.jar"]
--- src/main/java/com/nc/med/AppLauncher.java
+++ src/main/java/com/nc/med/AppLauncher.java
@@ -11,9 +11,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 @EnableScheduling
@@ -22,9 +28,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AppLauncher extends SpringBootServletInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(AppLauncher.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(AppLauncher.class);
+    }
 
+
+    public static boolean backup(String dbUsername, String dbPassword, String dbName, String outputFile)
+            throws IOException, InterruptedException {
+        String command = String.format("mysqldump -u%s -p%s --add-drop-table --databases %s -r %s",
+                dbUsername, dbPassword, dbName, outputFile);
+
+        Process process = Runtime.getRuntime().exec(command);
+        int processComplete = process.waitFor();
+        return processComplete == 0;
+    }
+
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    public void schedule() throws IOException, InterruptedException {
+        System.out.println("Backup Started at " + new Date());
+        Date backupDate = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd");
+        String dbNameList = "smart_001";
+        String saveFileName = format.format(backupDate) + "_smart_accounting_book_backup.sql";
+        String filePath = System.getProperty("user.home") + "/Documents/";
+        String savePath = filePath + File.separator + saveFileName;
+        backup("root", "Root@123", "smart_001", savePath);
     }
 
     @Override

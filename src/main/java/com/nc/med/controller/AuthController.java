8fb9a14b5142789504ba6d1c7c2ca5a6a177d4a2
Satish Chougule_2 <satish@dummy.com>
2022-03-20 01:20:43 +0530

    update user password

--- src/main/java/com/nc/med/controller/AuthController.java
+++ src/main/java/com/nc/med/controller/AuthController.java
@@ -20,13 +20,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -108,5 +106,17 @@ public class AuthController {
         user.setRoles(roles);
         userRepository.save(user);
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody SignupRequest signUpRequest) {
+        Optional<User> userOptional = userRepository.findByUsername(signUpRequest.getUsername());
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setPassword(encoder.encode(signUpRequest.getPassword()));
+            userRepository.save(user);
+            return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+        }
+        return ResponseEntity.ok(new MessageResponse("User doesn't exist"));
     }
 }
